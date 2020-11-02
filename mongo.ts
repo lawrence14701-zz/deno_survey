21702c4545451ace459c974659e894723d1f095d
LAWRENCE NICASTRO <lawrence.nicastro1@gmail.com>
2020-11-02 00:19:52 -0500

    connect mongo

--- mongo.ts
+++ mongo.ts
@@ -1,5 +1,7 @@
 import { MongoClient } from "./deps.ts";
 
 const client = new MongoClient();
-client.connectWithUri("");
+client.connectWithUri(
+  "mongodb+srv://dev:dev123@cluster0.hxzpp.mongodb.net/cluster0?retryWrites=true&w=majority"
+);
 const db = client.database("deno_survey");
